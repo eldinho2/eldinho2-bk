c2305da28e17158e97c090ce32564d4e2e31cae5
Vinicius Gonçalves <viniciusfod@gmail.com>
2022-01-27 16:05:17 -0300

    bk stranger

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
+const StrangerThingsRepository = require('./data/repository/StrangerThings');
+const StrangerThingsService = require('./services/StrangerThings');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const TheUpsideDown = process.env.UPSIDEDOWN_MODE;
+console.log(TheUpsideDown);
+
+const strangerThingsRepository = new StrangerThingsRepository(
+  strangerThingsDataset,
+);
+const strangerThingsService = new StrangerThingsService(
+  strangerThingsRepository,
+);
+
+app.use(cors());
+
+const hereIsTheUpsideDown = TheUpsideDown;
+
+app.get('/', (req, res) => {
+  const characters = strangerThingsService.search(
+    req.query,
+    hereIsTheUpsideDown,
+  );
+
+  res.status(200).json(characters);
+});
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
+});
